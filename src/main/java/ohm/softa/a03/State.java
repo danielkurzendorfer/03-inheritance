7553724534184c73954b20d93d2a2cadab0c6326
Daniel Kurzendorfer <daniel.kurzendorfer@gmail.com>
2019-04-07 11:24:49 +0200

    States hinzugefügt

--- src/main/java/ohm/softa/a03/State.java
+++ src/main/java/ohm/softa/a03/State.java
@@ -6,11 +6,22 @@ import org.apache.logging.log4j.Logger;
 public abstract class State {
     private static final Logger logger = LogManager.getLogger();
 
-    private int t;
+    private int time = 0;
     private int duration;
 
+    protected State(int duration) {
+        this.duration = duration;
+    }
+
     final State tick (Cat cat) {
-        return null;
+        time = time + 1;
+
+        if(time < duration) {
+            logger.info("still in {}", getClass().getSimpleName());
+            return this;
+        }
+        return successor(cat);
+
     }
 
     abstract State successor(Cat cat);
